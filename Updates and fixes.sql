696a4b458f6736a2546dbfa97ba80f4f73dffd46
TwistedHighway <TwistedHighway@users.noreply.github.com>
2018-10-17 07:28:00 -0600

    Delete Updates and fixes.sql

--- Updates and fixes.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM ELMAH_Error -- DELETE FROM Elmah_Error
-SELECT * FROM Log4Net_Error --DELETE FROM Log4Net_Error
-SELECT * FROM NLog_Error -- DELETE FROM NLog_Error
-SELECT * FROM vw_aspnet_WebEvents_extended --DELETE FROM aspnet_WebEvent_Events
